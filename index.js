10896acdc37cbd495a0c244b2fbf390d1abfa49a
ValiantWanderer <joe.craddock@outlook.com>
2023-05-02 23:16:13 +0100

    Added static route to render index html

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = reqiuire('express')
+const express = require('express')
 const app = express()
 const port = 3000
 
 
 
 app.get('/', (req, res) => {
-    res.send('Deez Nutz')
+    res.sendFile('./app/index.html', {root: __dirname})
+})
+
+app.post('/', (req, res) => {
+    let year = req.body.year;
+    let circuit = req.body.circuit;
+    let driver = req.body.driver;
+    let lap = req.body.lap;
+    getLap(year, circuit, driver, lap);
 })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
+
+
+async function getLap(year, circuit, driver, lap) {
+
+}
